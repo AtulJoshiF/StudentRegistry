7f4f31bbc91fd5f30e609bbd85371188d67aabc1
AtulJoshiF <123716513+AtulJoshiF@users.noreply.github.com>
2023-05-04 10:59:12 +0530

    Database Url
    The database URL has been updated

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost/Student_Registry_Application"
-
+SQL_ALCHEMY_DATABASE_URL = "postgresql://studentregistry_user:UoY9i5GtLIcoHwLkJggemowS8FfKfQYd@dpg-ch9jqsl269v0obam6b90-a.oregon-postgres.render.com/studentregistry"
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
